0cf3c0404f4c7f2fb4582cd99e72046008ee9892
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2021-04-03 16:51:05 -0300

    Adiciona tela inicial

--- Obscure/Obscure/ContentView.swift
+++ Obscure/Obscure/ContentView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Spacer()
+            VStack {
+                Text("Logo")
+            }
+            
+            Spacer()
+            
+            VStack{
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Criar sala")
+                        .foregroundColor(.white)
+                        
+                }.frame(width: 328, height: 62)
+                .background(Color.black)
+                .cornerRadius(10)
+                .padding(.top)
+                .padding(.leading)
+                .padding(.trailing)
+            
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Entrar numa sala")
+                        .foregroundColor(.white)
+                        
+                }.frame(width: 328, height: 62)
+                .background(Color.black)
+                .cornerRadius(10)
+                .padding()
+            }
+            
+            Spacer()
+            
+            VStack {
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Como jogar")
+                }
+            }.padding(.bottom)
+        }
     }
 }
 
