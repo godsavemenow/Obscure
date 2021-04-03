80e506a21d15322854e50b8d9f9d2386404ec4a0
godsavemenow <lvjs@cin.ufpe.br>
2021-04-03 15:58:54 -0300

    project upload

---
+++ Obscure/Obscure/ObscureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObscureApp.swift
+//  Obscure
+//
+//  Created by Lucas Silva on 03/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ObscureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
