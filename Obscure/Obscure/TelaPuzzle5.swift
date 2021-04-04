b4a6ed5ca1f809401410dfd75340d09818b077e2
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2021-04-04 17:05:00 -0300

    Atualiza tela do puzzle 5

--- Obscure/Obscure/TelaPuzzle5.swift
+++ Obscure/Obscure/TelaPuzzle5.swift
@@ -18,56 +18,18 @@ struct TelaPuzzle5: View {
             Color.black
                 .ignoresSafeArea()
             VStack {
-                Button(action: {
-                    if isPlaying == false {
-                        self.audioPlayer.play()}
-                    else {self.audioPlayer.stop()}
-                        
-                        self.isPlaying.toggle()}) {
-                    Image(self.isPlaying == true ? "pauseButton" : "playerButton")
-                        .resizable()
-                        .frame(width: 340, height: 105, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .scaledToFit()
-                        .padding(.bottom, 200)
-                }
+                Image("puzzle5")
+                    .resizable()
+                    .frame(width: 304, height: 117)
 
                
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 5, style: .continuous)
-                                    .fill(Color.white)
-                                    .frame(width: 328, height: 55)
-                    TextField("Código", text: $resposta)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .foregroundColor(.black)
-                        .frame(width: 300.0)
-                        .background(Color.white)
-                        .padding()
-                }
-                
-                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                    Text("Enviar resposta")
-                        .foregroundColor(.white)
-                        
-                }.frame(width: 328, height: 50)
-                .background(Color.red)
-                .cornerRadius(5)
-                .padding(.top)
-                .padding(.leading)
-                .padding(.trailing)
-                .padding(.bottom, 150)
-                
-                
-                }
-                
-            }.onAppear {
-                
-                let sound = Bundle.main.path(forResource: "rain-2", ofType: "mp3")
-                self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!)) }
+            
 
         }
     }
-
+    }
+}
+    
 struct TelaPuzzle5_Previews: PreviewProvider {
     static var previews: some View {
         TelaPuzzle5()
